baba9c7e2a5720f06d491f7c1b4e3fc2c84909e4
nieves <cesurmbg07@gmail.com>
2026-08-14 21:45:21 +0300

    Update character-card.js

--- character-card.js
+++ character-card.js
@@ -7,7 +7,7 @@
       age: "26 yaşında",
       location: "Amerika",
       occupation: "Şerif adayı",
-      imageUrl: "https://i.ibb.co/d4pvQD02/gfhdfg.png",
+      imageUrl: "https://i.ibb.co/MyP9ZcSH/gdfhsfdgh.png",
       avatar: "🐺",
       quote: "Yetkimi korkutmak için değil, güven vermek için kullanacağım.",
       story: `
